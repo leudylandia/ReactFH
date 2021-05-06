66dad40753e99dd11696849973a28c804627b49d
leudylandia <leudylandia26@hotmail.com>
2021-05-06 14:02:57 -0400

    Continuacion heroeapp

--- heroes-app/src/routers/AppRouter.js
+++ heroes-app/src/routers/AppRouter.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
     Switch, 
     Route
 } from 'react-router-dom'
+import { AuthContext } from '../auth/authContext'
 
 import { LoginScreen } from '../components/login/LoginScreen'
 import { DashboardRoutes } from './DashboardRoutes'
+import { PrivateRouter } from './PrivateRouter'
+import { PublicRouter } from './PublicRouter'
 
 export const AppRouter = () => {
+
+    const {user} = useContext(AuthContext);
+    console.log(user)
+
     return (
         <Router>
             <Switch>
-                <Route exact path="/login" component={LoginScreen}/>
-                <Route path="/" component={DashboardRoutes}/>
+                {/* <Route exact path="/login" component={LoginScreen}/> */}
+                <PublicRouter 
+                        exact path="/login" 
+                        component={LoginScreen}
+                        isAuthenticated={user.logged}
+                        />
+               
+                {/* <Route path="/" component={DashboardRoutes}/> */}
+                <PrivateRouter 
+                    path="/" 
+                    component={DashboardRoutes}
+                    isAuthenticated={user.logged}
+                />
             </Switch>
         </Router>
     )
